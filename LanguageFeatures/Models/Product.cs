45db9c184669754338de811e2bb677033695bf9c
typespt <65303033+typespt@users.noreply.github.com>
2021-12-14 21:02:15 +0300

    Add project files.

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public static Product[] GetProducts()
+        {
+            var kayak = new Product { Name = "Kayak", Price = 275M };
+            var lifejacket = new Product { Name = "lifejacket", Price = 49.95M };
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
